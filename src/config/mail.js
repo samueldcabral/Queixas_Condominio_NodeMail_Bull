ce97715444a973b3c73b412743c30ce8a5475d18
samuel <samueldcabral@gmail.com>
2020-12-09 12:32:25 -0300

    Mudada a caixa de email para a de Raimundo

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,21 +1,21 @@
-//Caixa de Email de Samuel no https://mailtrap.io/
-const mail = {
-  host: "smtp.mailtrap.io",
-  port: "2525",
-  auth: {
-    user: "487dd65d7ae706",
-    pass: "7814379c7a85f2"
-  },
-}
-
-//Caixa de Email de Raimundo no https://mailtrap.io/
+// //Caixa de Email de Samuel no https://mailtrap.io/
 // const mail = {
 //   host: "smtp.mailtrap.io",
 //   port: "2525",
 //   auth: {
-//     user: "49d58f45559090",
-//     pass: "97dcd9196011d8"
+//     user: "487dd65d7ae706",
+//     pass: "7814379c7a85f2"
 //   },
 // }
+
+// Caixa de Email de Raimundo no https://mailtrap.io/
+const mail = {
+  host: "smtp.mailtrap.io",
+  port: "2525",
+  auth: {
+    user: "49d58f45559090",
+    pass: "97dcd9196011d8"
+  },
+}
 
 module.exports = mail;
